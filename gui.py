8588c672d3485ed051944e8d7af9315c92876c0f
OrangeVulture <33159627+OrangeVulture@users.noreply.github.com>
2018-04-09 13:23:05 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import os
 
 grey = (211,211,211)
 red = (200,0,0)
@@ -9,6 +10,21 @@ b_green = (0, 255, 0)
 black = (0,0,0)
 blue = (0, 0, 255)
 
+def button(msg, x, y, w, h, ic, ac):
+
+	mouse = pygame.mouse.get_pos()
+
+	mybut = pygame.font.SysFont("Monospace",20)
+
+	if((x+w > mouse[0] > x) & (y+h > mouse[1] > y)):
+		pygame.draw.rect(gameDisplay, ac, (x,y,w,h))
+	else:
+		pygame.draw.rect(gameDisplay, ic, (x,y,w,h))
+		
+	textSurface, textRect = text_objects(msg,mybut)
+	textRect.center = (x+(w/2),y+(h/2))
+	gameDisplay.blit(textSurface, textRect)
+
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
@@ -17,12 +33,8 @@ def game_intro():
 	intro = True
 	clock = pygame.time.Clock()
 	myfont = pygame.font.SysFont("Monospace",75)
-	mybut = pygame.font.SysFont("Monospace",20)
-	display_width = 800
-	display_height = 600
 	
-	gameDisplay = pygame.display.set_mode((display_width,display_height))
-
+	
 	while intro :
 		for event in pygame.event.get():
 			# print(event)
@@ -36,49 +48,26 @@ def game_intro():
 		TextRect.center = ((display_width/2),(display_height/8))
 		gameDisplay.blit(TextSurf, TextRect)
 
-		mouse = pygame.mouse.get_pos()
-		# print(mouse)
-		if((325+140 > mouse[0] > 325) & (200+70 > mouse[1] > 200)):
-			pygame.draw.rect(gameDisplay, b_green, (325,200,140,70))
-		else:
-			pygame.draw.rect(gameDisplay, green, (325,200,140,70))
-		
-		textSurface, textRect = text_objects("PLAY",mybut)
-		textRect.center = (325+(140/2),200+(70/2))
-		gameDisplay.blit(textSurface, textRect)
+		button("PLAY",325,200,140,70,green,b_green)
 
-		if (325+140 > mouse[0] > 325) & (300+70 > mouse[1] > 300) :
-			pygame.draw.rect(gameDisplay, b_green, (325,300,140,70))
-		else:
-			pygame.draw.rect(gameDisplay, green, (325,300,140,70))
-		
-		textSurface, textRect = text_objects("INSTRUCTIONS",mybut)
-		textRect.center = (325+(140/2),300+(70/2))
-		gameDisplay.blit(textSurface, textRect)
+		button("INSTRUCTIONS",325,300,140,70,green,b_green)
 
-		if (325+140 > mouse[0] > 325)&(400+70>mouse[1]>400):
-			pygame.draw.rect(gameDisplay, b_green, (325,400,140,70))
-		else:
-			pygame.draw.rect(gameDisplay, green, (325,400,140,70))
-		
-		textSurface, textRect = text_objects("CREDITS",mybut)
-		textRect.center = (325+(140/2),400+(70/2))
-		gameDisplay.blit(textSurface, textRect)
+		button("CREDITS",325,400,140,70,green,b_green)
 
-		if (325+140 > mouse[0] > 325)&(500+70>mouse[1]>500):
-			pygame.draw.rect(gameDisplay, b_red, (325,500,140,70))
-		else:
-			pygame.draw.rect(gameDisplay, red, (325,500,140,70))
-
-		textSurface, textRect = text_objects("EXIT",mybut)
-		textRect.center = (325+(140/2),500+(70/2))
-		gameDisplay.blit(textSurface, textRect)
+		button("EXIT",325,500,140,70,red,b_red)
 
 		pygame.display.update()
 		clock.tick(15)
 
+os.environ['SDL_VIDEO_CENTERED'] = '1'					#To position the pygame window at the center
+display_width = 800
+display_height = 600
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+
 pygame.init()
 
 game_intro()
+
+
 
 pygame.exit()
