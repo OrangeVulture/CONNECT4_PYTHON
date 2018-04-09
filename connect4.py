0a82c419278f4a93756be81d245658885711af48
OrangeVulture <33159627+OrangeVulture@users.noreply.github.com>
2018-04-09 16:20:42 +0530

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -3,14 +3,40 @@ import pygame				#Main game runs with this library
 import sys 					#sys.exit()
 import math 				#For floor function
 import os 					#For environ function, to position window at center
+import time
 
-ROW = 6
-COLUMN = 7
+#COLOR CONSTANTS
 BLUE = (0, 0, 255)			#RGB 
 BLACK = (0, 0, 0)
+red = (200,0,0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 GOLD = (255,215,0)
+green = (0, 200, 0)
+GREEN = (0, 255, 0)
+B_RED = (255, 0, 0)
+GREY = (211,211,211)
+
+#PROGRAM CONSTANTS
+ROW = 6
+COLUMN = 7
+
+#
+os.environ['SDL_VIDEO_CENTERED'] = '1'					#To position the pygame window at the center
+SQUARESIZE = 100
+width = COLUMN * SQUARESIZE
+height = (ROW+1) * SQUARESIZE				#+1 FOr DISPLAY SCREEN which shows where the chip is to be dropped
+RADIUS = int(SQUARESIZE/2 -5)
+size = (width, height)
+screen = pygame.display.set_mode(size)
+
+display_width = 800
+display_height = 600
+# gameDisplay = pygame.display.set_mode((width,height))
+
+
+
+#GAME FUNCTIONS
 
 def create_board():
 	board = np.zeros((ROW,COLUMN))       	#Putting zero in all the rows and columns
@@ -65,94 +91,163 @@ def draw_board(board):
 				pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2) , height - int(r*SQUARESIZE + SQUARESIZE/2)), RADIUS)
 	
 	pygame.display.update()
-			
-
-board = create_board()                 		#Initialise the board(First time)
-print_board(board)							
-
-game_over = False 							#Game_Flag 
-turn = 0									#Player_Flag 
-draw_game = int(0)							#DRAW GAME CHECK
-
-os.environ['SDL_VIDEO_CENTERED'] = '1'				#To position the pygame window at the center
-
-pygame.init()
-
-SQUARESIZE = 100
-width = COLUMN * SQUARESIZE
-height = (ROW+1) * SQUARESIZE				#+1 FOr DISPLAY SCREEN which shows where the chip is to be dropped
-
-size = (width, height)
-RADIUS = int(SQUARESIZE/2 -5)
-
-screen = pygame.display.set_mode(size)
-draw_board(board)
-pygame.display.update()
-
-myfont = pygame.font.SysFont("Monospace",75)
-draw = pygame.font.SysFont("Monospace",75)
-
-while not game_over:
-	for event in pygame.event.get(): 			#detects any movement from mouse
-		if(event.type == pygame.QUIT):
-			sys.exit()
-
-		if(event.type == pygame.MOUSEMOTION):
-			pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
-			posx = event.pos[0]
-			if turn == 0 :
-				pygame.draw.circle(screen,RED,(posx, int (SQUARESIZE/2)),RADIUS)
-			else :
-				pygame.draw.circle(screen,YELLOW,(posx, int(SQUARESIZE/2)),RADIUS)
-			pygame.display.update()
-
-		if(event.type == pygame.MOUSEBUTTONDOWN):
-			pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
-			if turn == 0:                    		#Player 1(turn = even or 0)
-				posx = event.pos[0]					
-				col = int(math.floor(posx/SQUARESIZE)) 		#Detects column to be put in
-
-				if is_valid_location(board,col):
-				 	row = get_next_open_row(board,col)
-				 	drop_piece(board,row,col,1)
-
-				 	if(chicken_dinner(board,1)):
-				 		label = myfont.render("Player 1 wins!!",1,RED)
-				 		screen.blit(label, (40,10))
-				 		game_over = True
-
-			else:                                 #Player 2(turn = odd or 1)
-				posx = event.pos[0]					
-				col = int(math.floor(posx/SQUARESIZE))    #Detects column to be put in
-
-				if is_valid_location(board,col):
-				 	row = get_next_open_row(board,col)
-				 	drop_piece(board,row,col,2)
-
-				 	if(chicken_dinner(board,2)):
-				 		label = myfont.render("Player 2 wins!!",1,YELLOW)
-				 		screen.blit(label, (40,10))
-				 		game_over = True 
-
-			#For checking draw game condition
-			draw_game = int(draw_game + 1)
-			if(draw_game == ROW*COLUMN):
-				#pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
-				label = draw.render("GAME DRAW!!!!",1,GOLD)
-				screen.blit(label, (40,10))
-				game_over = True
-
-			print_board(board)
-			draw_board(board)
-
-			turn += 1
-			turn = turn % 2
-
-			if game_over:
-				pygame.time.wait(3000)
-
-			
 
 
+
+#GUI FUNCTIONS
+
+
+def button(msg, x, y, w, h, ic, ac, act="NONE"):
+	mouse = pygame.mouse.get_pos()
+	click = pygame.mouse.get_pressed()	
+
+	mybut = pygame.font.SysFont("Monospace",20)
+
+	if((x+w > mouse[0] > x) & (y+h > mouse[1] > y)):
+		pygame.draw.rect(screen, ac, (x,y,w,h))      #gameDisplay
+		if(click[0] == 1 and act != "NONE"):
+			if(act == "PLAY"):
+				game_loop()
+			elif(act == "INST"):
+				pass
+			elif(act == "CRED"):
+				pass
+			elif(act == "EXIT"):
+				pygame.quit()
+				quit()
+	else:
+		pygame.draw.rect(screen, ic, (x,y,w,h))		#gameDisplay
+		
+	textSurface, textRect = text_objects(msg,mybut)
+	textRect.center = (x+(w/2),y+(h/2))
+	screen.blit(textSurface, textRect) 				#gameDisplay
+
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, BLACK)
+    return textSurface, textSurface.get_rect()
+
+
+def game_intro():
+
+	pygame.init()
+
+	intro = True
+	clock = pygame.time.Clock()
+	myfont = pygame.font.SysFont("Monospace",75)
+
+	# os.environ['SDL_VIDEO_CENTERED'] = '1'					#To position the pygame window at the center
 
 	
+	
+	while intro :
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+		
+		screen.fill(GREY)											#gameDisplay
+		
+		TextSurf, TextRect = text_objects("CONNECT-4", myfont)
+		TextRect.center = ((width/2),(height/8))
+		screen.blit(TextSurf, TextRect)								#gameDisplay
+
+		button("PLAY",280,200,140,70, green, GREEN,"PLAY")
+
+		button("INSTRUCTIONS",280,300,140,70, green, GREEN,"INST")
+
+		button("CREDITS",280,400,140,70, green, GREEN,"CRED")
+
+		button("EXIT",280,500,140,70, red, RED,"EXIT")
+
+		pygame.display.update()
+		clock.tick(5)
+
+
+
+			
+			
+def game_loop():						#Function in loop to run the game
+	
+	# os.environ['SDL_VIDEO_CENTERED'] = '1'				#To position the pygame window at the center
+
+	board = create_board()                 		#Initialise the board(First time)
+	print_board(board)				
+
+	
+
+	pygame.display.update()
+	draw_board(board)
+	pygame.display.update()
+
+	myfont = pygame.font.SysFont("Monospace",75)
+	# draw = pygame.font.SysFont("Monospace",75)
+
+	game_over = False 							#Game_Flag 
+	turn = 0									#Player_Flag 
+	draw_game = int(0)							#DRAW GAME CHECK
+
+	while not game_over:
+		for event in pygame.event.get(): 			#detects any movement from mouse
+			if(event.type == pygame.QUIT):
+				sys.exit()
+
+			if(event.type == pygame.MOUSEMOTION):
+				pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+				posx = event.pos[0]
+				if turn == 0 :
+					pygame.draw.circle(screen,RED,(posx, int (SQUARESIZE/2)),RADIUS)
+				else :
+					pygame.draw.circle(screen,YELLOW,(posx, int(SQUARESIZE/2)),RADIUS)
+				pygame.display.update()
+
+			if(event.type == pygame.MOUSEBUTTONDOWN):
+				pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+				if turn == 0:                    		#Player 1(turn = even or 0)
+					posx = event.pos[0]					
+					col = int(math.floor(posx/SQUARESIZE)) 		#Detects column to be put in
+
+					if is_valid_location(board,col):
+					 	row = get_next_open_row(board,col)
+					 	drop_piece(board,row,col,1)
+
+					 	if(chicken_dinner(board,1)):
+					 		label = myfont.render("Player 1 wins!!",1,RED)
+					 		screen.blit(label, (40,10))
+					 		game_over = True
+
+				else:                                 #Player 2(turn = odd or 1)
+					posx = event.pos[0]					
+					col = int(math.floor(posx/SQUARESIZE))    #Detects column to be put in
+
+					if is_valid_location(board,col):
+					 	row = get_next_open_row(board,col)
+					 	drop_piece(board,row,col,2)
+
+					 	if(chicken_dinner(board,2)):
+					 		label = myfont.render("Player 2 wins!!",1,YELLOW)
+					 		screen.blit(label, (40,10))
+					 		game_over = True 
+
+				#For checking draw game condition
+				draw_game = int(draw_game + 1)
+				if(draw_game == ROW*COLUMN):
+					#pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+					label = myfont.render("GAME DRAW!!!!",1,GOLD)
+					screen.blit(label, (40,10))
+					game_over = True
+
+				print_board(board)
+				draw_board(board)
+
+				turn += 1
+				turn = turn % 2
+
+				if game_over:
+					pygame.time.wait(3000)
+
+				
+
+
+game_intro()
+		
